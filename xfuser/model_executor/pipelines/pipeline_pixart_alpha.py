464afeab75738d6924743994c270379b4a2f7774
duihuhu <duihuhu@163.com>
2024-08-13 09:56:53 +0800

    debug breakdown time

--- xfuser/model_executor/pipelines/pipeline_pixart_alpha.py
+++ xfuser/model_executor/pipelines/pipeline_pixart_alpha.py
@@ -155,7 +155,7 @@ class xFuserPixArtAlphaPipeline(xFuserPipelineBaseWrapper):
         if "mask_feature" in kwargs:
             deprecation_message = "The use of `mask_feature` is deprecated. It is no longer used in any computation and that doesn't affect the end results. It will be removed in a future version."
             deprecate("mask_feature", "1.0.0", deprecation_message, standard_warn=False)
-        print("aaaaaaaa")
+        print("aaaaaaaa\n")
         # 1. Check inputs. Raise error if not correct
         height = height or self.transformer.config.sample_size * self.vae_scale_factor
         width = width or self.transformer.config.sample_size * self.vae_scale_factor
