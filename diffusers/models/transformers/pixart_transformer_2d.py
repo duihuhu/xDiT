8b4433cdc4d1c7a7dd0f8b4058cb20f6dfdcc021
duihuhu <duihuhu@163.com>
2024-08-13 10:27:21 +0800

    debug breakdown time

--- diffusers/models/transformers/pixart_transformer_2d.py
+++ diffusers/models/transformers/pixart_transformer_2d.py
@@ -105,7 +105,7 @@ class PixArtTransformer2DModel(ModelMixin, ConfigMixin):
         attention_type: Optional[str] = "default",
     ):
         super().__init__()
-        print("init pioxart model ")
+        logger.error("init pioxart model ")
         # Validate inputs.
         if norm_type != "ada_norm_single":
             raise NotImplementedError(
@@ -332,5 +332,5 @@ class PixArtTransformer2DModel(ModelMixin, ConfigMixin):
 
         if not return_dict:
             return (output,)
-
+        logger.error("Transformer2DModelOutput ")
         return Transformer2DModelOutput(sample=output)
