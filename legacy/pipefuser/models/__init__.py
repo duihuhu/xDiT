a3a0573e3a5666f1652d1596a36604737ebe8522
duihuhu <duihuhu@163.com>
2024-08-21 09:56:35 +0800

    debug breakdown time

--- legacy/pipefuser/models/__init__.py
+++ legacy/pipefuser/models/__init__.py
@@ -9,5 +9,5 @@ from .distri_dit_pp import DistriDiTPP
 from .distri_dit_tp import DistriDiTTP
 from .distri_dit_pipefusion import DistriDiTPipeFusion
 
-if Version("0.30.0.dev0") <= Version(diffusers.__version__):
-    from .distri_dit_hunyuan_pipefusion import DistriDiTHunyuanPipeFusion
+# if Version("0.30.0.dev0") <= Version(diffusers.__version__):
+from .distri_dit_hunyuan_pipefusion import DistriDiTHunyuanPipeFusion
